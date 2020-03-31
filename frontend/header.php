97aa384b8b4440df2c6a57d7152bd5639de6a1d6
Jenia Isler <jenia.isler@gmx.ch>
2020-03-31 10:34:26 +0200

    added config header

---
+++ frontend/header.php
@@ -0,0 +1,9 @@
+<?php include_once("../db/db_connect.php") ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Noteboard</title> 
+        <link href="css/style.css" rel="stylesheet">
+        <?php require "../bootstrap/bootstap_temp.php"; ?>
+    </head>
