198f0eb596584907d7ef200bb74e4441f486c197
Vasan Sivanesan <vasan.sivanesan01@gmail.com>
2020-03-31 10:50:11 +0200

    first steps login and registration

--- frontend/login_frontend.php
+++ frontend/login_frontend.php
@@ -1,99 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-form {border: 3px solid #f1f1f1;}
-
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
-
-button {
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
-
-button:hover {
-  opacity: 0.8;
-}
-
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #f44336;
-}
-
-.imgcontainer {
-  text-align: center;
-  margin: 24px 0 12px 0;
-}
-
-img.avatar {
-  width: 40%;
-  border-radius: 50%;
-}
-
-.container {
-  padding: 16px;
-}
-
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
-}
-</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php require "../bootstrap/bootstap_temp.php"; ?>
 </head>
 <body>
+    <div style="width: 50%; text-align: center; margin: auto">
+        <h2>Login Form</h2>
+        <form action="/action_page.php" method="post">
+              <div class="imgcontainer">
+                <img src="avatar.png" alt="Avatar" class="avatar">
+              </div>
 
-<h2>Login Form</h2>
-
-<form action="/action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="avatar.png" alt="Avatar" class="avatar">
-  </div>
-
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-        
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
-
+              <div class="form-group">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" class="form-control" placeholder="Enter Username" name="uname" required>
+              </div>
+              <div class="form-group">
+                <label for="psw"><b>Password</b></label>
+                <input type="password" class="form-control" placeholder="Enter Password" name="psw" required>
+              </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+              <div class="form-group form-check">    
+                <label>
+                  <input type="checkbox" class="form-check-input" checked="checked" name="remember"> Remember me
+                </label>
+              </div>    
+              <div class="form-group" style="background-color:#f1f1f1">
+                <button type="button" class="badge badge-pill badge-light">Cancel</button>
+                <span class="badge badge-pill badge-light">Forgot <a href="#">password?</a></span>
+              </div>
+        </form>
+    </div>
 </body>
 </html>
         
