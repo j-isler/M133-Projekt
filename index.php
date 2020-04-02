56fddb5767800e0fffb6ad532f5707d58b937cc5
Jenia Isler <jenia.isler@gmx.ch>
2020-04-02 17:25:49 +0200

    restructured to usable state

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Noteboard</title> 
+        <link href="css/style.css" rel="stylesheet">
+        <?php require "../bootstrap/bootstap_temp.php"; ?>
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand">Noteboard</a>
+            <button class="btn btn-link my-2 my-sm-0" type="submit">Login</button>
+        </nav>
+        
+    </body>
+</html>
