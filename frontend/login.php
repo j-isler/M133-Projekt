f5c1f9b9f09140fcd921f8cd10801553cb1efe99
Jenia Isler <jenia.isler@gmx.ch>
2020-04-07 18:31:55 +0200

    deleted useless files

--- frontend/login.php
+++ frontend/login.php
@@ -1,3 +1,39 @@
+<?php
+   include_once("../authentication/function.php");
+   include_once("../db/db_connect.php");
+   
+
+
+         if(!func::checkLoginState($dbh)){
+          
+            if (isset($_POST['username']) && isset($_POST['password'])){
+
+               $query = "SELECT * From users Where username = :username And password = :password";
+               
+               $username = $_POST['username'];
+               $password = $_POST['password'];
+
+
+               $stmt = $dbh->prepare($query);
+               $stmt->execute(array(':username' => $username, ':password' => $password));
+
+               $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+               if ($row['id'] > 0){
+                  
+                  func::createRecord($dbh, $row['id'], $row['username']);
+                  header("location: ../NoteBoard/index.php");
+                  
+
+               }
+
+
+            }
+         }else{
+            header("location: ../NoteBoard/index.php");
+         }
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +44,7 @@
 </head>
 <body>
     <div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;"> 
-        <form action="/action_page.php" method="post" style="border:1px solid #ccc; border-radius: 15px; padding: 20px">
+        <form action="login.php" method="post" style="border:1px solid #ccc; border-radius: 15px; padding: 20px">
              <h2>Login</h2>
               <div class="imgcontainer" style="padding: 5px">
                 <img src="avatar.png" alt="Avatar" class="avatar">
@@ -16,11 +52,11 @@
 
               <div class="form-group">
                 <label for="uname"><b>Username</b></label>
-                <input type="text" class="form-control" placeholder="Enter Username" name="uname" required>
+                <input type="text" class="form-control" placeholder="Enter Username" name="username" required>
               </div>
               <div class="form-group">
                 <label for="psw"><b>Password</b></label>
-                <input type="password" class="form-control" placeholder="Enter Password" name="psw" required>
+                <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
               </div>
                 <button type="submit" class="btn btn-primary">Login</button>
               <div class="form-group form-check">    
