49ce47db86c99d208db3b0ffb81dfc0416d384f7
Jenia Isler <jenia.isler@gmx.ch>
2020-04-07 18:34:34 +0200

    added noteboard page

---
+++ Noteboard/index.php
@@ -0,0 +1,22 @@
+<?php
+    include_once("../authentication/function.php");
+    include_once("../db/db_connect.php");
+
+    if(!func::checkLoginState($dbh)){
+        header("location:../frontend/login_frontend.php");
+       // exit();
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Willkommen " . $_SESSION['username'] . " auf der Noteboardapplikation!"; ?>
+    <p>hier geht es raus: </p> <a href="../authentication/logout.php"> Logout </a>
+</body>
+</html>
