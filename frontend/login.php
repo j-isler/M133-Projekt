3ead6c1d3d53a42e5d1c5b36b1968b84fd01b4ef
Jenia Isler <jenia.isler@gmx.ch>
2020-04-28 10:23:58 +0200

    fixed vasans schissdreck

--- frontend/login.php
+++ frontend/login.php
@@ -1,11 +1,3 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php require "../bootstrap/bootstap_temp.php"; ?>
-    <link href="css/style.css" rel="stylesheet">
-</head>
 <?php
   include_once("header.php");
 ?>
