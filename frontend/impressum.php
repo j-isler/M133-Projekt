b8a577a5fed29a5d1a680e8ab1bb0ada50038d4a
Jenia Isler <jenia.isler@gmx.ch>
2020-04-28 10:29:38 +0200

    removed checkloginstate from impressum

--- frontend/impressum.php
+++ frontend/impressum.php
@@ -1,6 +1,11 @@
-<?php
-  include_once("header.php");
-?>
+<!DOCTYPE html>
+     <html>
+     <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link href="css/style.css" rel="stylesheet">
+         <link href="css/style.css" rel="stylesheet">
+         <?php require("../bootstrap/bootstap_temp.php"); ?>
+     </head>
 <body>
     <p>Name: Noteboard</p>
     <p>Adresse: Ausstellungsstrasse 70, 8005 Zürich</p>
