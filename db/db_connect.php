e595d8efbcb89f347f9d7598ceff38a8bdb4e156
Jenia Isler <jenia.isler@gmx.ch>
2020-04-30 09:46:28 +0200

    removed legacy SQL driver

--- db/db_connect.php
+++ db/db_connect.php
@@ -1,17 +1,5 @@
 <?php
-   
-   
-   // legacy db connect needs to be changed as soosn as register is rewritten
-   define('DB_SERVER', 'db:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'mariadb');
-   define('DB_DATABASE', 'noteboard');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
-
-   
-   
-   
    ini_set('display_errors',1);
    ini_set('display_startup_errors', 1);
    error_reporting(E_ALL);
