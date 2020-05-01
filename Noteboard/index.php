6ac619a4bb4b747890a12622de9759b35a426fb9
Vasan Sivanesan <vasan.sivanesan01@gmail.com>
2020-05-01 16:29:38 +0200

    noteboard design

--- Noteboard/index.php
+++ Noteboard/index.php
@@ -36,26 +36,31 @@
             <a href="/" class="navbar-brand">Noteboard</a>
             <a href="../authentication/logout.php"><button class="btn btn-link my-2 my-sm-0" type="submit">Logout</button></a>
     </nav>
-    <div>
+    <div class="card" style="width: 60rem; postion: fixed; text-align: center;">
+    <div class="card-header">
         <?php echo "Willkommen " . $_SESSION['username'] . " auf der Noteboardapplikation!"; ?>
     </div>
-    <button type="button" class="btn btn-primary" id="toggleNoteEntry">Toggle Entry</button>
-    <div id="EntryForm">
+    <div class="card-body">
+    <button type="button" class="btn btn-primary" id="toggleNoteEntry" style="width: 5rem;">Toggle Entry</button>
+    <div id="EntryForm" class="card">
         <form action="src/create-record.php" method="post">
-            <textarea class="form-control" name="text" id="NoteText"" rows="3"></textarea>
+            <textarea class="form-control" name="text" id="NoteText" rows="3" class="card-text"></textarea>
             <button class="btn btn-info" type="submit">Add</button>
         </form>
+    </div>
     </div>
 
     <?php
         foreach( (array) $rows as $row):
             
     ?>
-    <div>
+    <div class="card-body" style="border: 2px solid black; border-radius: 10px;">
         <p><?php echo $row['text']; ?></p>
-        <form action="src/delete-record.php" method="post"> <button type="submit" name="noteid" value="<?php echo $row['id']; ?>">Delete</button> </form>
+        <form action="src/delete-record.php" method="post" > <button class="btn btn-light"  type="submit" name="noteid" value="<?php echo $row['id']; ?>">Delete</button> </form>
     </div>
+    
         <?php endforeach; ?>
+    </div>
     
         
 
--- db/db_connect.php
+++ db/db_connect.php
@@ -5,7 +5,7 @@
    error_reporting(E_ALL);
    
 
-   $dbh = new PDO('mysql:host=db;dbname=noteboard', 'root', 'mariadb');
+   $dbh = new PDO('mysql:host=localhost;dbname=noteboard', 'root', 'mariadb');
 
  /*
    $stmt = $dbh->prepare("Select * From users;");
